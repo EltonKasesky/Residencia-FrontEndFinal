d8cf6cbb9b88889662c6cc8a56917cc374bb2fe1
EltonKasesky <eltonkasesky9@gmail.com>
2025-05-08 16:25:58 -0300

    Local project

---
+++ src/js/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const travelRoutes = require('../routes/travel');
+
+const app = express();
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use('/css', express.static(path.join(__dirname, '..', 'css')));
+app.use('/js', express.static(path.join(__dirname)));
+app.use('/img', express.static(path.join(__dirname, '..', 'img')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
+app.use('/api/travel', travelRoutes);
+
+app.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000');
+});
